6f16d5ec061996c5a17250fa30c15a2283629de8
Edith Cruz <143680095+edithvcruz@users.noreply.github.com>
2025-06-09 01:37:45 -0600

    Add files via upload

---
+++ reactivo/Pizzeria.java
@@ -0,0 +1,15 @@
+
+package modulo2.tecnolochicas.reactivo;
+
+import java.util.Arrays;
+import java.util.List;
+public class Pizzeria {
+    public static List<String> ingredientes = Arrays.asList("🍍", "🍄", "🧅", "🍗", "🥩", "🧀", "🫑", "🫒");
+
+    public static void iniciar() {
+        Mostrador.tomarPedidos()
+                .subscribe(pizza -> {
+                    System.out.println(pizza);
+                });
+    }
+}
---
+++ reactivo/ReactivoApplication.java
@@ -0,0 +1,19 @@
+package modulo2.tecnolochicas.reactivo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivoApplication.class, args);
+
+	}
+@Override
+	
+	public void run(String... args) {
+		Pizzeria.iniciar();
+}
+}
